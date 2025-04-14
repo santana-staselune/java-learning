3c3809a491df9f23cac07ae444e15879c7a6fce3
Santana S <santana.voinikevica@gmail.com>
2025-04-14 19:06:52 +0300

    [feature/lesson_1]: Learn Java Variables

--- src/main/java/lv/acodemy/SecondLesson.java
+++ src/main/java/lv/acodemy/SecondLesson.java
@@ -2,21 +2,4 @@ package lv.acodemy;
 
 public class SecondLesson {
 
-    String name = "Santana";
-    int age = 32;
-
-    //array
-    int[] numbers = new int[5];
-    String[] names = {"John", "Andrey"}; //[John, Andrey]
-
-    int[] ages = {19, 20, 30, 41, 50};
-    String[] fruitBasket = new String[10];
-
-    int[] ages = {19, 20, 30, 41, 50};
-// index: [0] - 19
-
-// Element access
-        System.out.println(numbers[0]);
-    numbers[0]=10;
-        System.out.println(Arrays.toString(numbers));
 }
